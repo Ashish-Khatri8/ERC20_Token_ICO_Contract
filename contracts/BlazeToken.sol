9c8946ce990da4a7e37ec068c4e12e61b06bcd7d
Ashish Khatri <khatri8.ashish@gmail.com>
2022-05-10 18:47:16 +0530

    Changed token decimals to 9

--- contracts/BlazeToken.sol
+++ contracts/BlazeToken.sol
@@ -9,4 +9,8 @@ contract BlazeToken is Ownable, ERC20 {
         ERC20(name, symbol) {
             _mint(owner(), supply * 10**9);
     }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 9;
+    }
 }
