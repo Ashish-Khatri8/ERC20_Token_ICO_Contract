0738526e811e4a4c8eb1d188cb84474c4044fc54
Ashish Khatri <khatri8.ashish@gmail.com>
2022-05-10 16:47:22 +0530

    Changed token decimals to 9

--- contracts/BlazeToken.sol
+++ contracts/BlazeToken.sol
@@ -7,6 +7,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract BlazeToken is Ownable, ERC20 {
     constructor(uint256 supply, string memory name, string memory symbol) 
         ERC20(name, symbol) {
-            _mint(owner(), supply * 10**decimals());
+            _mint(owner(), supply * 10**9);
     }
 }
